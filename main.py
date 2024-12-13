5ed77838d9a3ba8d643e8ad2c076eb20d826b3e0
Den-Chel <denis.chechulin.31@gmail.com>
2024-12-12 20:09:28 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Written by: Denis Chechulin
+Date: Nov 8, 2024
+"""
+
+str1 = "Hello World"
+str2 = "hello world"
+
+# Check if str1 is equal to str2 or not
+if str1 != str2:
+    print("The strings are not equal")
+if str1 == str2:
+    print("The strings are equal")
